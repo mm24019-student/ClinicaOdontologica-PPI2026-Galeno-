62dd1174346198421ac5dac818abefe68d3020fc
GA20025 <GA20025@ues.edu.sv>
2026-09-19 16:44:17 -0600

    Agrega OrdenExamenDAO

---
+++ src/main/java/sv/ues/edu/occ/ingenieria/pp115_2026/salud/galenosv/control/OrdenExamenDAO.java
@@ -0,0 +1,21 @@
+package sv.ues.edu.occ.ingenieria.pp115_2026.salud.galenosv.control;
+
+import jakarta.ejb.LocalBean;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.io.Serializable;
+import sv.ues.edu.occ.ingenieria.pp115_2026.salud.galenosv.entity.OrdenExamen;
+
+@Stateless
+@LocalBean
+public class OrdenExamenDAO extends DefaultDAO<OrdenExamen> implements Serializable {
+
+    @PersistenceContext(unitName = "Galeno-PU")
+    EntityManager em;
+
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+}
